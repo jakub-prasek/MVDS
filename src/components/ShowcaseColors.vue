8e5cd7818047bca3b4d25a29f1eec95922bd6a7e
Jakub Prasek <jakub.prasek@runecast.com>
2021-12-02 21:04:18 +0100

    add

--- src/components/ShowcaseColors.vue
+++ src/components/ShowcaseColors.vue
@@ -1,47 +1,57 @@
 <template>
-    <div>
-        <!-- Color Scope -->
-        <section 
-        v-for="color in colors" 
-        :key="color"
-        class="color-block"
-        >
-            <!-- Shade Scope -->
-            <div 
-            v-for="shade in shades" 
-            :key="shade"
-            class="shade-block"
-            :style= "{'background-color': 'hsla(var(--def-color-'+ color +'),'+ shade +'%)', }">
-
-                {{ shade }}
-                <span>{{ color }}</span>
-
-            </div>
-            <!-- End Shade Scope -->
-        </section>
-        <!-- End Color Scope -->
-    </div>
+  <div>
+    <!-- Color Scope -->
+    <section v-for="color in colors" :key="color" class="color-block">
+      <!-- Shade Scope -->
+      <div
+        v-for="shade in shades"
+        :key="shade"
+        class="shade-block"
+        :style="{
+          'background-color':
+            'hsla(var(--def-color-' + color + '),' + shade + '%)',
+        }"
+      >
+        <div v-if="shade > 50" class="shade-block_label">
+          <span>{{ color }}</span>
+          <span>{{ shade }}</span>
+        </div>
+        <div v-else class="shade-block_label shade-block_label-light">
+            <span>{{ color }}</span>
+            <span>{{ shade }}</span>
+        </div>
+      </div>
+      <!-- End Shade Scope -->
+    </section>
+    <!-- End Color Scope -->
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                colors: ["accent","neutral","link"],
-                shades: [10,20,30,40,50,60,70,80,90,95,100]
-            }
-        },
-    }
+export default {
+  data() {
+    return {
+      colors: ["accent", "neutral", "link"],
+      shades: [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100],
+    };
+  },
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .color-block {
-    display: flex;
+  display: flex;
 }
 .shade-block {
-    flex: 1 1;
+  flex: 1 1;
+  display: flex;
+  flex-direction: column;
+}
+.shade-block_label {
     display: flex;
     flex-direction: column;
 }
-
+.shade-block_label-light {
+  color: white;
+}
 </style>
